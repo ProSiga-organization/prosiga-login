829c316293bc3f954aceb7900b7dec27c28db96d
manuvaladares <manuella.valadares@hotmail.com>
2025-10-15 09:45:29 -0300

    fix: corrige a configuração das portas e a comunicação com o back rodando

--- app/main.py
+++ app/main.py
@@ -2,11 +2,11 @@ from fastapi import FastAPI
 from .login.router import router as login_router
 from .database import Base, engine
 
-Base.metadata.create_all(bind=engine) # Garante que o serviço "conhece" as tabelas
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="ProSiga Auth API")
 app.include_router(login_router)
 
 @app.get("/")
 def health_check():
-    return {"status": "ok"}
+    return {"status": "fine"}
