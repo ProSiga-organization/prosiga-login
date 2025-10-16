b8e8f0c370ac42fc934f54bc16440ec807a8716b
manuvaladares <manuella.valadares@hotmail.com>
2025-10-16 11:41:20 -0300

    feat: Implementa serviço de autenticação separado

--- app/login/router.py
+++ app/login/router.py
@@ -1,5 +1,4 @@
-from fastapi import APIRouter, Depends, HTTPException, status
-from fastapi.security import OAuth2PasswordRequestForm
+from fastapi import APIRouter, Depends, HTTPException, status, Form
 from sqlalchemy.orm import Session
 from ..database import get_db
 from ..security import verify_password, create_access_token, get_current_user
@@ -8,31 +7,20 @@ from .. import model
 from . import schema
 
 router = APIRouter(prefix="/login", tags=["Login"])
-
-# --- ENDPOINT DE LOGIN ---
 @router.post("/", response_model=schema.TokenResponse)
-def login_for_access_token(db: Session = Depends(get_db), form_data: OAuth2PasswordRequestForm = Depends()):
+def login_for_access_token(db: Session = Depends(get_db), username: str = Form(), password: str = Form()):
     repo = UsuarioRepository()
-    # Usa o repositório local para encontrar o usuário
-    user = repo.find_by_email(db, email=form_data.username)
+    user = repo.find_by_email(db, email=username)
     
-    # Verifica a senha e o status
-    if not user or not user.status == model.StatusContaEnum.ATIVO or not verify_password(form_data.password, user.senha_hash):
+    if not user or not user.status == model.StatusContaEnum.ATIVO or not verify_password(password, user.senha_hash):
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="Email ou senha incorretos, ou usuário não ativo.",
         )
     
-    # Cria o token se as credenciais estiverem corretas
     access_token = create_access_token(data={"sub": user.email})
     return {"access_token": access_token, "token_type": "bearer"}
 
-# --- ENDPOINT DE VERIFICAÇÃO DE TOKEN ---
 @router.get("/me", response_model=schema.UsuarioResponse)
 def read_users_me(current_user: model.Usuario = Depends(get_current_user)):
-    """
-    Este endpoint é usado por outros serviços para validar um token.
-    Se o token enviado no cabeçalho for válido, ele retorna os dados do usuário.
-    Se for inválido, a função 'get_current_user' irá retornar um erro 401.
-    """
     return current_user
