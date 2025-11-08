9da3b9497c607ade9455244f88b0d41128918850
manuvaladares <manuella.valadares@hotmail.com>
2025-11-08 17:24:28 -0300

    feat: adiciona CORS middleware para permitir requisições do front

--- app/main.py
+++ app/main.py
@@ -1,10 +1,23 @@
 from fastapi import FastAPI
 from .login.router import router as login_router
 from .database import engine, Base
+from fastapi.middleware.cors import CORSMiddleware
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="ProSiga Auth API", description="API dedicada para autenticação.")
+
+origins = [
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,       
+    allow_credentials=True,      
+    allow_methods=["*"],         
+    allow_headers=["*"],         
+)
 
 app.include_router(login_router)
 
